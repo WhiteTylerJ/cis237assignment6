dd0bab3be089ee60df30becbe046338002948e5f
Tyler <white_tylerj@live.com>
2015-12-13 16:08:55 -0500

    Changed Home page, about page, and contact page

--- cis237Assignment6/Controllers/HomeController.cs
+++ cis237Assignment6/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace cis237Assignment6.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Assignment 6 - Beverage Database Details";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact the creator";
 
             return View();
         }
